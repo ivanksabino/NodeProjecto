fd868f520e6afefcd677906d65553a6c86abfc55
ivan sabino <ivanksabino@gmail.com>
2021-02-24 18:48:46 +0000

    baga

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
     res.send("Lar doce lar")
 
 });
 
-const port = process.env.port || 8080
+const port = process.env.port || 8080;
 app.listen(port, () => {
     console.log("Olha eu aquiiii nessa bagaça");
 });

dca483c1ef0bc38502a54914fe16349c6f4e84bb
ivan sabino <ivanksabino@gmail.com>
2021-02-25 17:38:53 +0000

    80811

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
 
 app.get('/', (req, res) => {
     res.send("Lar doce lar");

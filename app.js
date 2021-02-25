1c8c58d55acfea0da3bc8880ee40e9af5823a2a8
ivan sabino <ivanksabino@gmail.com>
2021-02-25 11:23:56 +0000

    remoce healthchecks

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ app.get('/', (req, res) => {
     res.send("Lar doce lar");
 
 });
-app.get('/healthCheck', (req, res) => {
-    res.send("Check");
-
-});
 
 const port = process.env.port || 8080;
 app.listen(port, () => {
@@ -17,9 +13,3 @@ app.listen(port, () => {
 
 var http = require('http');
 
-http.createServer(onRequest_a).listen(80);
-
-function onRequest_a (req, res) {
-    res.write('healthCheck');
-    res.end();
-  }

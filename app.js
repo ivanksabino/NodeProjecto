1cc80e5e6dc9ad89daed6ecbb8d3eee473b89dcc
ivan sabino <ivanksabino@gmail.com>
2021-02-26 18:23:28 +0000

    new endpoint

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const { response } = require('express');
+const db = require('./queries')
 
 app.use(bodyParser.json())
 app.use(
@@ -14,10 +15,10 @@ app.get('/', (req, res) => {
     return res.json({message: 'CHEGA JUNTO ANDORNHO'});
 
 });
+app.get('/all', db.getAll)
 
 const port = 3333;
 app.listen(port, () => {
     console.log("Olha eu aquiiii nessa bagaça");
 });
-
 
--- queries.js
+++ queries.js
@@ -3,9 +3,21 @@ require('dotenv').config({ path: '.env' })
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    user: "ilitch",
+    host: "nodeprojecto.cjkvvca6w7zu.eu-west-2.rds.amazonaws.com",
+    database: "postgres",
+    password: "ilitch123",
+    port: "5432",
 })
+
+const getAll = (request, response) => {
+    pool.query('SELECT * FROM testando', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+module.exports = {
+    getAll
+}

bea4f3e4423c48d15204519423bfd7d1d671bb34
ivan sabino <ivanksabino@gmail.com>
2021-03-01 12:55:39 +0000

    subindo novo formato de data

--- sqlPost.py
+++ sqlPost.py
@@ -2,7 +2,7 @@ import postgresql
 from datetime import datetime
 
 db = postgresql.open('pq://ilitch:ilitch123@nodeprojecto.cjkvvca6w7zu.eu-west-2.rds.amazonaws.com:5432/postgres')
-dateTime =  datetime.now().strftime("%H:%M:%S")
+dateTime =  datetime.now().strftime("%H:%M:%S %d %m %Y")
 print(dateTime)
 for i in range(0,15):
     sql = """UPDATE public.testando \

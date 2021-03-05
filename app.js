7e0f65852b253199db6d154054a3a79b1e7dae39
Ivan Sabino <ivanksabino@gmail.com>
2021-03-05 14:05:33 +0000

    add post

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const { response } = require('express');
-const db = require('./queries')
+const db = require('./queries');
+const { Pool } = require('pg/lib');
 
 app.use(bodyParser.json())
 app.use(
@@ -15,10 +16,22 @@ app.get('/', (req, res) => {
     return res.json({message: 'CHEGA JUNTO ANDORNHO'});
 
 });
-app.get('/all', db.getAll)
+app.get('/all', db.getAll);
+
+app.post("/add", async (req, res) =>{
+    try{
+        const { description } = req.body;
+        const newUser = await db.pool.query(
+            "INSERT INTO public.testando('userName', 'userDate') VALUES ('ïvanInserido', 'Foi um post');"
+        );
+        }catch(err){
+            console.error(err.message)
+        }
+})
 
 const port = 3333;
 app.listen(port, () => {
     console.log("Olha eu aquiiii nessa bagaça");
 });
 
+    
